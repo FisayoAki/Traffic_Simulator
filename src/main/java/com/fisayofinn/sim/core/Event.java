9a1752edaff05e5eaf8dcd0a37e324a6a917265f
gitDemo <finn.kinsella@ucdconnect.ie>
2025-11-03 21:58:13 +0000

    Comments added to Event.java

--- src/main/java/com/fisayofinn/sim/core/Event.java
+++ src/main/java/com/fisayofinn/sim/core/Event.java
@@ -1,19 +1,24 @@
 package com.fisayofinn.sim.core;
 
+/** Event objects */
 public class Event implements Comparable<Event>{
+
 
     private double time;
     private EventType type;
     private int userID;
 
+    // Constructor
     public Event(double time, EventType type, int userID) {
         this.time = time;
         this.type = type;
         this.userID = userID;
     }
 
+    // Compare Event objects to find which has priority
     @Override
     public int compareTo(Event other) {
+        // If equal timing, prioritise lower userID
         if (this.time == other.time){
             return this.userID < other.userID ? 1 : -1;
         }else {
