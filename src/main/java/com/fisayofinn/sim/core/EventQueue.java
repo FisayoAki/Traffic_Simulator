7f084698778c9bc19b24bd804b5e053c257ca71e
gitDemo <finn.kinsella@ucdconnect.ie>
2025-11-03 21:43:50 +0000

    Outline of EventQueue and some of its methods

--- src/main/java/com/fisayofinn/sim/core/EventQueue.java
+++ src/main/java/com/fisayofinn/sim/core/EventQueue.java
@@ -1,4 +1,25 @@
 package com.fisayofinn.sim.core;
 
+import java.util.PriorityQueue;
+
 public class EventQueue {
+
+    // One priority queue per simulation so it is final
+    private final PriorityQueue<Event> eventQueue;
+
+    // Constructor
+    public EventQueue(){
+        this.eventQueue = new PriorityQueue<Event>();
+    }
+
+    // Add event to queue
+    public void addEvent(Event myEvent){
+        eventQueue.add(myEvent);
+    }
+
+    // Check if queue has no events to proceed with
+    public boolean isEmpty(){
+        return eventQueue.isEmpty()
+    }
+
 }
