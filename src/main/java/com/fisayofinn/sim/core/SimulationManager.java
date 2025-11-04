5496270e9cc66a3fc89f30db6b53afdb84557bbf
gitDemo <finn.kinsella@ucdconnect.ie>
2025-11-04 11:56:25 +0000

    SimulationManager outline of Event processing

--- src/main/java/com/fisayofinn/sim/core/SimulationManager.java
+++ src/main/java/com/fisayofinn/sim/core/SimulationManager.java
@@ -1,4 +1,64 @@
 package com.fisayofinn.sim.core;
 
+import com.fisayofinn.sim.sources.OnOffSource;
+import org.w3c.dom.events.EventException;
+
+import java.util.ArrayList;
+import java.util.*;
+
+
+
 public class SimulationManager {
+
+    private EventQueue eventQueue;
+    private ArrayList<OnOffSource> sources;
+    private double simTime, endTime;
+    private int activeSources;
+
+
+
+    public SimulationManager(){
+        eventQueue = new EventQueue();
+        // Create the END event from user input
+        eventQueue.addEvent(new Event(endTime, EventType.END, -1));
+
+
+    }
+
+    public void simLoop(){
+        while(simTime < endTime && !eventQueue.isEmpty()){
+            Event nextEvent = eventQueue.retrieveEvent();
+            updateSimTime(nextEvent.getTime());
+            processEvent(nextEvent);
+        }
+    }
+
+    private void processEvent(Event event){
+        switch(event.getType()){
+            case ON:
+                //switch User state
+                // increment sources count
+                // add new Event
+                break;
+            case OFF:
+                //switch User state
+                // increment sources count
+                // add new Event
+                break;
+            case RECORD:
+                // record state (maybe to a map)
+                // add new Record Event (if using intervals)
+                break;
+            case END:
+                // do not process any more events
+                // end simulation
+                break;
+
+        }
+    }
+
+    private void updateSimTime(double eventTime){
+        simTime = eventTime;
+    }
+
 }
