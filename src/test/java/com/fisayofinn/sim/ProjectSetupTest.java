e055066a96e8faa21adeb28b57c2637a672b2263
Fisayo <fisayoaki04@gmail.com>
2025-12-10 16:58:52 +0000

    Set up JUnit 5 test

---
+++ src/test/java/com/fisayofinn/sim/ProjectSetupTest.java
@@ -0,0 +1,13 @@
+package com.fisayofinn.sim;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class ProjectSetupTest {
+
+    @Test
+    public void testJUnitIsWorking() {
+        assertTrue(true);
+    }
+}
